0a883e27a59455079e0916cee8e1c402f0453ad2
Kevin <kevinunger98@gmail.com>
2019-09-29 18:31:55 +0200

    Initial sharing of project

---
+++ Punkt.java
@@ -0,0 +1,80 @@
+/**
+ * Beschreiben Sie hier die Klasse Punkt.
+ * 
+ * @author Emil Hofmann, Charlotte Geißler 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+import java.lang.Math;
+public class Punkt
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    public int x;
+    public int y;
+    /**
+     * Konstruktor für Objekte der Klasse Punkt
+     */
+    public Punkt()
+    {
+   
+       
+    }
+      //Konstruktor 1 nimmt x und y entgegen und setzt diese in x,y ein
+    public Punkt(int x , int y)
+    {
+        
+         this.x = x;
+         this.y = y;        
+
+     } 
+         
+
+    
+    public int getY()
+    {
+       
+        return this.y;
+    }
+    public int getX()
+    {
+       
+        return this.x; 
+    }
+    public void setX (int x)
+    {
+        this.x = x;
+        
+        
+    
+    }
+    public void setY (int y)
+    {
+        this.y = y;
+        
+       
+    }
+        
+    public void aktuellePositionAusgeben()
+    {
+        System.out.println ("X= " + x + "  "+ "Y= " + y);
+       
+    }
+    public void bewegenUm( int dx, int dy)
+    { 
+        setX(x + dx);
+        setY(y + dy);
+    
+    }   
+    public double gibAbstand(int x2, int y2)
+    {
+        int x1 = getX();
+        int y1 = getY();
+        
+        return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+    
+    }
+  
+        
+        
+        
+        
+}
---
+++ Rechteck.java
@@ -0,0 +1,119 @@
+/**
+ * Beschreiben Sie hier die Klasse Rechteck.
+ * 
+ * @author Emil Hofamnn, Charlotte Geißler
+ * @version Alpha_2
+ */
+import java.awt.Color;
+import javax.swing.JOptionPane;
+public class Rechteck
+{
+    public int breite;
+    public int laenge;
+    public String bezeichnung;
+    //public float r,g,b;
+    private Color farbe;
+    Punkt position = new Punkt(30,20);
+    public int v1;
+    public int v2;
+    
+    public Rechteck() 
+    {
+    
+    
+    }
+    public Rechteck(Punkt position, int breite, int laenge,
+    String bezeichnung)
+    {
+    
+    }
+    public int getBreite()
+    {
+       
+        return this.breite;
+    }
+    public int getLaenge()
+    {
+        return this.laenge; 
+    }
+    public String bezeichnung()
+    {
+        return bezeichnung;
+    
+    }
+    public Punkt position()
+    {
+        return position;
+        
+    }
+    
+    
+    public void setBreite (int breite)
+    {
+    
+        this.breite = breite;
+              
+    }
+    public void setLaenge (int laenge)
+    {
+        this.laenge = laenge;
+    
+    }    
+    public void setPosition (Punkt position)
+    {
+        this.position = position;
+    
+    }
+    public void setBezeichnung (String bezeichnung)
+    {
+        this.bezeichnung = bezeichnung;
+    
+    }
+     public void setColor(int r, int g, int b)
+    {
+       
+       if (r>255 || g>255 || b>255)
+       { JOptionPane.showMessageDialog(null, "Wert liegt nicht im Definitionsbereich!", "Fehlermeldung", JOptionPane.PLAIN_MESSAGE);
+           
+        }
+       else 
+       {farbe = new Color(r, g, b);}
+       
+    }
+    
+  
+    public void bewegeUm (int dx, int dy)
+    {
+        position.x = position.x + dx;
+        position.y = position.y + dy;
+    
+    }
+    /*public void bewegenUm (int vx, int vy)
+    {
+        position.x = position.x + vx;
+        position.y = position.y + vy;
+        
+    }*/
+    public void bewegeUm(Punkt verschiebevektor)
+    {
+        position.x = verschiebevektor.x + position.x;
+        position.y = verschiebevektor.y + position.y;
+    }
+    public void ausgabeAttribute()
+   
+    {
+        System.out.println("Breite: " + breite);
+        System.out.println("Länge: " + laenge);
+        System.out.println("Position: " + position.x + " ; "+position.y);
+        System.out.println("Bezeichnung: " + bezeichnung);
+        //System.out.println("Farbe: " + r + " ; " +  g + " ; " + b );
+        System.out.println(farbe); 
+    }
+   
+   
+   
+    
+    
+    
+  
+}
